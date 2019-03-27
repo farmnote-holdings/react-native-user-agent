603d3fcf3e60dccd19ea0374e30762aecdf502ac
masayo ito <m-ito@farmnote.jp>
2019-03-27 13:26:45 +0900

    ビルドが通らないのでメソッドを追加した

--- android/src/main/java/com/bebnev/RNUserAgentPackage.java
+++ android/src/main/java/com/bebnev/RNUserAgentPackage.java
@@ -21,4 +21,8 @@ public class RNUserAgentPackage implements ReactPackage {
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
 }
